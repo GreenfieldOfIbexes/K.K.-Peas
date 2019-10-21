1edc1d2cb8671e546216d6f6df3ea3b701494f1f
lrbarr11 <lrbarr11@gmail.com>
2019-10-21 14:55:31 -0400

    modified server/index.js for correct static route

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = express();
 
 const port = 3000;
 
-app.use(express.static(__dirname + "/../client/dist"))
+app.use(express.static(__dirname + "/../dist"))
 
 app.listen(port, () => {
     console.log(`app is listening on port ${port}`)
