bef902c97a593fc91f9ce414cc21f548b4c7dd3d
Harry Shapiro <harrydshapiro96@gmail.com>
2019-10-22 19:39:10 -0400

    Added provider and all that jazz

--- src/index.js
+++ src/index.js
@@ -4,13 +4,12 @@ import App from "./components/App.jsx";
 import store from "./store";
 import newMainProduct from "./actions/newMainProduct";
 import { Provider } from "react-redux";
-import store from "./store.js";
 
 store.dispatch(newMainProduct(1));
 
 ReactDOM.render(
-	<Provider>
-		<App store={store} />
+	<Provider store={store}>
+		<App />
 	</Provider>,
 	document.getElementById("app"),
 );
