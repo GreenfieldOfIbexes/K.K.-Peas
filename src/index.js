fa5e8d164470134c85ab1a0734c1084ada957633
Harry Shapiro <harrydshapiro96@gmail.com>
2019-10-22 19:26:33 -0400

    Added provider to index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom";
 import App from "./components/App.jsx";
 import store from "./store";
 import newMainProduct from "./actions/newMainProduct";
-// adding to enable async/await with babel
-const babelRegister = require("babel-core/register");
-const babelPolyfill = require("babel-polyfill");
+import { Provider } from "react-redux";
+import store from "./store.js";
 
 store.dispatch(newMainProduct(1));
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(
+	<Provider>
+		<App store={store} />
+	</Provider>,
+	document.getElementById("app"),
+);
