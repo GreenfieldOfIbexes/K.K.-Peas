3dd368297104be6307d78e15181fda6e7a9efdbd
lrbarr11 <lrbarr11@gmail.com>
2019-10-23 11:07:06 -0400

    added files for R_R component, added R_R component to app, renamed some files in R_R

---
+++ src/components/R_R/ReviewList.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const ReviewList = () => (
+    <div>
+        ReviewList
+    </div>
+)
+
+export default ReviewList;
---
+++ src/components/R_R/ReviewSummery.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ReviewSummery = () => (
+    <div>
+        ReviewSummery
+    </div>
+)
+
+export default ReviewSummery
