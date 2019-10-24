acac3c3e72fcaecb6b79abe35672e128fb52d870
john <john.otwell@protonmail.com>
2019-10-24 19:38:39 -0400

    Implemented a card component

--- tests/testing.test.js
+++ tests/testing.test.js
@@ -1,7 +1,3 @@
 import makeProductObj from "../src/actions/makeProductObj-possibleRefile.js";
 const util = require("util");
-
-test("adds 1 + 2 to equal 3", async () => {
-	let returnVal = await makeProductObj(7);
-	console.log(util.inspect(returnVal, false, null, true));
-});
+test("", () => {});
