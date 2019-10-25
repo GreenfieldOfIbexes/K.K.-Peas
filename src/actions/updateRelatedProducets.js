c591b6d1dea773a2c1e17d4e70b73fedcf13a643
john <john.otwell@protonmail.com>
2019-10-25 14:42:40 -0400

    updateRelatedProducts references store

--- src/actions/updateRelatedProducets.js
+++ src/actions/updateRelatedProducets.js
@@ -2,9 +2,8 @@ const getProductInfo = require("../actions/makeProductObj-possibleRefile.js");
 
 const updateRelatedProducts = (relatedProductIds) => {
 	return (dispatch) => {
-		console.log(getProductInfo);
 		relatedProductIds.forEach(async (id) => {
-			const product = await getProductInfo(id);
+			const product = await getProductInfo.default(id);
 			dispatch({
 				type: "UPDATE_RELATED_PRODUCTS",
 				product: product,
