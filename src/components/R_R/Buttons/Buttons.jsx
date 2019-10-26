b682b18f6fc89a0f55cb1712adf9df96c83cc5be
lrbarr11 <lrbarr11@gmail.com>
2019-10-26 11:35:07 -0400

    add review now pulls up window for a new review, rating stars dont work and submitting doesnt do anything

--- src/components/R_R/Buttons/Buttons.jsx
+++ src/components/R_R/Buttons/Buttons.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Buttons.css'
-import {Modal} from 'react-bootstrap'
+import {Modal, Form} from 'react-bootstrap'
+import Stars from '../Stars.jsx'
 
 class Buttons extends React.Component {
     constructor(props){
@@ -32,21 +33,55 @@ class Buttons extends React.Component {
 
 
     render() {
+        console.log("buttons props:", this.props.reviews)
+        const characteristics = ["Size", "Width", "Comfort", "Quality", "Length", "Fit"]
         return (
         <div className="review-buttons">
 
-            <Modal show={this.state.show} onClose={this.close}>
-                <Modal.header closeButton>
+            <Modal show={this.state.show} onHide={this.close}>
+                <Modal.Header closeButton>
                     <Modal.Title>
                         New Review
                     </Modal.Title>
-                </Modal.header>
+                </Modal.Header>
                 <Modal.Body>
-                    stuff
+                    <Form>
+                        <Form.Group>
+                            <Form.Label>Username</Form.Label>
+                            <Form.Control type="text" placeholder="Enter Username"/>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Title</Form.Label>
+                            <Form.Control type="text" placeholder="Enter Review Title"/>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Description</Form.Label>
+                            <Form.Control as="textarea" placeholder="Enter Review Description"/>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Category Ratings</Form.Label>
+                            {characteristics.map((char) =>{
+                                return (
+                                    <Form.Group>
+                                        <Form.Label style={{marginRight: "5%", marginLeft: "5%"}}>{char}:</Form.Label>
+                                        <Form.Check type="radio" inline label="1"/>
+                                        <Form.Check type="radio" inline label="2"/>
+                                        <Form.Check type="radio" inline label="3"/>
+                                        <Form.Check type="radio" inline label="4"/>
+                                        <Form.Check type="radio" inline label="5"/>
+                                    </Form.Group>
+                                )
+                            })}
+                        </Form.Group>
+                        <Form.Group style={{justifyContent: 'center', display: "flex"}}>
+                            <div style={{display: 'flex', justifyContent: "stretch"}}>Rating</div>
+                            <Stars />
+                        </Form.Group>
+                    </Form>
                 </Modal.Body>
                 <Modal.Footer>
-                    <button onClick={this.close}>close</button>
-                    <button>submit</button>
+                    <button onClick={this.close}>Close</button>
+                    <button>Submit</button>
                 </Modal.Footer>
             </Modal>
 
