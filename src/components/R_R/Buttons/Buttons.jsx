bf6b6f493010467375eb1e122989b48e575b6922
lrbarr11 <lrbarr11@gmail.com>
2019-10-26 12:24:17 -0400

    add review form almost done, stars need to work, and need to make put request on submit

--- src/components/R_R/Buttons/Buttons.jsx
+++ src/components/R_R/Buttons/Buttons.jsx
@@ -8,11 +8,23 @@ class Buttons extends React.Component {
         super(props)
 
         this.state={
-            show: false
+            show: false,
+            starValue: 0,
+            userName: '',
+            Size: null,
+            Width: null,
+            Comfort: null,
+            Quality: null,
+            Length: null,
+            Fit: null
         }
         this.addReview = this.addReview.bind(this)
         this.getReviews = this.getReviews.bind(this)
         this.close = this.close.bind(this)
+        this.handleChar = this.handleChar.bind(this)
+        this.handleDescription = this.handleDescription.bind(this)
+        this.handleTitle = this.handleTitle.bind(this)
+        this.handleUsername = this.handleUsername.bind(this)
     }
 
     getReviews() {
@@ -31,13 +43,40 @@ class Buttons extends React.Component {
         })
     }
 
+    handleUsername(e){
+        this.setState({
+            userName: e.target.value
+        })
+        console.log('username: ', e.target.value)
+    }
+
+    handleTitle(e) {
+        this.setState({
+            title: e.target.value
+        })
+        console.log("title: ", e.target.value)
+    }
+
+    handleDescription(e) {
+        this.setState({
+            description: e.target.value
+        })
+        console.log('description: ', e.target.value)
+    }
+
+    handleChar(e, char){
+        var obj = {};
+        obj[e.target.name] = e.target.value;
+        this.setState(obj)
+        console.log("e and char: ", e.target.value, e.target.name)
+    }
+
 
     render() {
         console.log("buttons props:", this.props.reviews)
         const characteristics = ["Size", "Width", "Comfort", "Quality", "Length", "Fit"]
         return (
         <div className="review-buttons">
-
             <Modal show={this.state.show} onHide={this.close}>
                 <Modal.Header closeButton>
                     <Modal.Title>
@@ -48,35 +87,79 @@ class Buttons extends React.Component {
                     <Form>
                         <Form.Group>
                             <Form.Label>Username</Form.Label>
-                            <Form.Control type="text" placeholder="Enter Username"/>
+                            <Form.Control type="text" placeholder="Enter Username" onChange={this.handleUsername}/>
                         </Form.Group>
                         <Form.Group>
                             <Form.Label>Title</Form.Label>
-                            <Form.Control type="text" placeholder="Enter Review Title"/>
+                            <Form.Control type="text" placeholder="Enter Review Title" onChange={this.handleTitle}/>
                         </Form.Group>
                         <Form.Group>
                             <Form.Label>Description</Form.Label>
-                            <Form.Control as="textarea" placeholder="Enter Review Description"/>
+                            <Form.Control as="textarea" placeholder="Enter Review Description" onChange={this.handleDescription}/>
                         </Form.Group>
                         <Form.Group>
                             <Form.Label>Category Ratings</Form.Label>
                             {characteristics.map((char) =>{
                                 return (
                                     <Form.Group>
-                                        <Form.Label style={{marginRight: "5%", marginLeft: "5%"}}>{char}:</Form.Label>
-                                        <Form.Check type="radio" inline label="1"/>
-                                        <Form.Check type="radio" inline label="2"/>
-                                        <Form.Check type="radio" inline label="3"/>
-                                        <Form.Check type="radio" inline label="4"/>
-                                        <Form.Check type="radio" inline label="5"/>
+                                        <Form.Label style={{marginRight: "100%", marginLeft: "5%"}}>{char}:</Form.Label>
+                                            <div style={{display: "flex", justifyContent: 'center'}}>
+                                                <Form.Check
+                                                type="radio"
+                                                label="1"
+                                                name={char}
+                                                id="formHorizontalRadios1"
+                                                value={1}
+                                                onClick={this.handleChar}
+                                                inline
+                                                />
+                                                <Form.Check
+                                                type="radio"
+                                                label="2"
+                                                name={char}
+                                                id="formHorizontalRadios1"
+                                                value={2}
+                                                onClick={this.handleChar}
+                                                inline
+                                                />
+                                                <Form.Check
+                                                type="radio"
+                                                label="3"
+                                                name={char}
+                                                id="formHorizontalRadios1"
+                                                value={3}
+                                                onClick={this.handleChar}
+                                                inline
+                                                />
+                                                <Form.Check
+                                                type="radio"
+                                                label="4"
+                                                name={char}
+                                                id="formHorizontalRadios1"
+                                                value={4}
+                                                onClick={this.handleChar}
+                                                inline
+                                                />
+                                                <Form.Check
+                                                type="radio"
+                                                label="5"
+                                                name={char}
+                                                id="formHorizontalRadios1"
+                                                value={5}
+                                                onClick={this.handleChar}
+                                                inline
+                                                />
+                                            </div>
                                     </Form.Group>
                                 )
                             })}
                         </Form.Group>
                         <Form.Group style={{justifyContent: 'center', display: "flex"}}>
-                            <div style={{display: 'flex', justifyContent: "stretch"}}>Rating</div>
-                            <Stars />
+                            <div style={{width: '100%'}}>Rating</div>
                         </Form.Group>
+                        <div style={{justifyContent: 'center', display: "flex"}}>
+                            <Stars value={this.state.starValue}/>
+                        </div>
                     </Form>
                 </Modal.Body>
                 <Modal.Footer>
