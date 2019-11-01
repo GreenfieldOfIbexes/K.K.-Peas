42888ac9ab5a9fa4fc68319a7dafa636880f12df
Harry Shapiro <harrydshapiro96@gmail.com>
2019-11-01 15:16:50 -0400

    asdasd

--- src/components/R_R/ReviewList/Review.jsx
+++ src/components/R_R/ReviewList/Review.jsx
@@ -11,68 +11,6 @@ import { NavigationFullscreenExit } from "material-ui/svg-icons";
 const Review = (props) => {
 	var date = moment(props.review.date).format("MMM DD, YYYY");
 	const summary = props.review.summary;
-	const [body, setBody] = useState(props.review.body);
-	console.log("body length", body.length);
-	if (body.length > 250) {
-		setBody(props.review.body.slice(0, 250));
-	}
-
-	return (
-		<>
-			<div className="review-header-container">
-				<div>
-					<Stars rating={props.review.rating} />
-				</div>
-				<div>
-					{props.review.reviewer_name}, {date}
-				</div>
-			</div>
-			<div className="review-title">{summary}</div>
-			<div className="review-description">{body}</div>
-			{body.length === 250 ? (
-				<div
-					onClick={() => {
-						setBody(props.review.body);
-					}}
-					style={{ display: "flex" }}>
-					expand
-				</div>
-			) : (
-				<div />
-			)}
-			{props.review.photos.map((photo) => {
-				return <Photos photo={photo} />;
-			})}
-			{props.review.response ? (
-				<Response response={props.review.response} />
-			) : (
-				<div />
-			)}
-			<div className="footer">
-				{props.review.recommend === 1 ? (
-					<div className="review-recommend">
-						I recommend this product <i className="material-icons">check</i>
-					</div>
-				) : (
-					<div />
-				)}
-				<div className="footer-buttons">
-					<div className="helpful">
-						<Helpful review={props.review} />
-					</div>
-					<div className="report">
-						<Report review={props.review} />
-					</div>
-				</div>
-			</div>
-			<div className="review-line" />
-		</>
-	);
-};
-
-const Review = (props) => {
-	var date = moment(props.review.date).format("MMM DD, YYYY");
-	const summary = props.review.summary;
 	// const [body, setBody] = useState(props.review.body)
 	// if(body.length > 250){
 	//     setBody(props.review.body.slice(0, 250))
