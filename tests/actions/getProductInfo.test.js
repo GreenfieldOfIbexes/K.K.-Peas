587e04cf31327bfd51112bfc4eee49056014935b
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-05 15:18:32 +0800

    Delete tests directory

--- tests/actions/getProductInfo.test.js
+++
@@ -1,8 +0,0 @@
-import getProductInfo from "../../src/actions/getProductInfo.js";
-import { testProduct } from "../testProducts.js";
-
-test("should get a product object when given id", async () => {
-	// expect.assertions(1);
-	// const product = await getProductInfo(1);
-	// return expect(product).toEqual(testProduct);
-});
--- tests/testing.test.js
+++
@@ -1,6 +0,0 @@
-import makeProductObj from "../src/actions/makeProductObj-possibleRefile.js";
-const util = require("util");
-test("", async () => {
-	// let product = await makeProductObj(2);
-	// console.log(util.inspect(product, { depth: null }));
-});
