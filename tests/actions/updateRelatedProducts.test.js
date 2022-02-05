b163a5a65a3c5365ed20917a8b07682a9f2ef314
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-05 15:33:02 +0800

    Delete tests directory

--- tests/actions/updateRelatedProducts.test.js
+++
@@ -1,8 +0,0 @@
-import { testRelatedProducts } from "../testProducts.js";
-import updateRelatedProducts from "../../src/actions/updateRelatedProducets.js";
-
-test("related products action object is well-formed and corresponds to product 1's related object", async () => {
-	// const expectedRelProducts = testRelatedProducts;
-	// const actualRelProducts = await updateRelatedProducts(1);
-	// expect(actualRelProducts).toEqual(expectedRelProducts);
-});
